dee3867cdd4a8adff87f22a9b4d869cfb6fa4d99
Mahad Kalam <55807755+SkyfallWasTaken@users.noreply.github.com>
2025-07-12 15:31:20 +0000

    Step 1

--- src/index.ts
+++ src/index.ts
@@ -15,5 +15,36 @@ const db = new Keyv(
   })
 );
 
+app.command("/memegen", async ({ command, ack, respond, say }) => {
+  await ack();
+
+  if (!command.text) {
+    await respond("Please provide some text for the meme.");
+    return;
+  }
+
+  const res = await fetch("https://api.imgflip.com/caption_image", {
+    method: "POST",
+    body: new URLSearchParams({
+      username: process.env.IMGFLIP_USERNAME!,
+      password: process.env.IMGFLIP_PASSWORD!,
+      template_id: "181913649",
+      text0: command.text,
+    }),
+  });
+
+  const json: any = await res.json();
+
+  await say({
+    blocks: [
+      {
+        type: "image",
+        image_url: json.data.url,
+        alt_text: "delicious tacos",
+      },
+    ],
+  });
+});
+
 await app.start();
 console.log("We're up and running :)");
